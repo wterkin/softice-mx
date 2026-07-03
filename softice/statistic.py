ce63ae3610f5c0f4889239ac7269b256ebcf90ae
Andrey Pakhomenkov <oakhollow@yandex.ru>
2026-07-03 16:41:44 +0300

    Переделка статистика, в целом, завершена

--- softice/statistic.py
+++ softice/statistic.py
@@ -51,30 +51,6 @@ DATABASE_NAME: str = "softice.db"
 ERROR_CODE: int = -1
 
 
-"""
-   content_dict = parsed_dict["content"]
-
-    if content_dict["msgtype"] == "m.text":
-        event = RoomMessageText.from_dict(parsed_dict)
-    elif content_dict["msgtype"] == "m.emote":
-        event = RoomMessageEmote.from_dict(parsed_dict)
-    elif content_dict["msgtype"] == "m.notice":
-        event = RoomMessageNotice.from_dict(parsed_dict)
-    else:
-        event = RoomMessageUnknown.from_dict(parsed_dict)
-
-RoomMessageVideo(source={'unsigned': {'membership': 'join'}, 'content': {'body': 'we_and_penguin.mp4', 'info': {'duration': 5459, 'h': 768, 'mimetype': 'video/mp4', 'size': 511978, 'thumbnail_info': {'h': 600, 'mimetype': 'image/jpeg', 'size': 175495, 'w': 400}, 'thumbnail_url': 'mxc://sibnsk.net/OGFIGpTYtfhHoeQZsKdSdjMU', 'w': 512, 'xyz.amorgan.blurhash': 'TpJ8kaRP?b-;M{Rj~qWBM{t7t7ax'}, 'm.mentions': {}, 'msgtype': 'm.video', 'url': 'mxc://sibnsk.net/idvjZMIwupHYsEgndCyrEpas'}, 'origin_server_ts': 1782996328242, 'sender': '@namo:sibnsk.net', 'type': 'm.room.message', 'event_id': '$jRckPbSoq8PIIb84k7oAExq2ugoxJY7Y4fEz07y0PjQ'}, event_id='$jRckPbSoq8PIIb84k7oAExq2ugoxJY7Y4fEz07y0PjQ', sender='@namo:sibnsk.net', server_timestamp=1782996328242, decrypted=False, verified=False, sender_key=None, session_id=None, transaction_id=None, url='mxc://sibnsk.net/idvjZMIwupHYsEgndCyrEpas', body='we_and_penguin.mp4')
-RoomMessageImage(source={'unsigned': {'membership': 'join'}, 'content': {'body': 'getImage.gif', 'info': {'h': 720, 'mimetype': 'image/gif', 'org.matrix.msc4230.is_animated': False, 'size': 23346, 'w': 938, 'xyz.amorgan.blurhash': 'LCSF;LWA%M_3-;?bIU9Z~qNGM{WA'}, 'm.mentions': {}, 'msgtype': 'm.image', 'url': 'mxc://sibnsk.net/oekOtgYvuWqSwZNWDatKLHcO'}, 'origin_server_ts': 1782996112426, 'sender': '@namo:sibnsk.net', 'type': 'm.room.message', 'event_id': '$42UhlIsI-depk-UfEfW4bofEm73X7-OiVgs-fzDJl9c'}, event_id='$42UhlIsI-depk-UfEfW4bofEm73X7-OiVgs-fzDJl9c', sender='@namo:sibnsk.net', server_timestamp=1782996112426, decrypted=False, verified=False, sender_key=None, session_id=None, transaction_id=None, url='mxc://sibnsk.net/oekOtgYvuWqSwZNWDatKLHcO', body='getImage.gif')
-RoomMessageAudio(source={'unsigned': {'membership': 'join'}, 'content': {'body': 'forest_sounds.flac', 'info': {'duration': 243294, 'mimetype': 'audio/flac', 'size': 31217543}, 'm.mentions': {}, 'msgtype': 'm.audio', 'url': 'mxc://sibnsk.net/zwYAqRoIKqNCeEFCrcKCGscb'}, 'origin_server_ts': 1782996432895, 'sender': '@namo:sibnsk.net', 'type': 'm.room.message', 'event_id': '$3klkKhwO8KdkNaWvJpf62bc2J8xvhO8u7REySdcY2do'}, event_id='$3klkKhwO8KdkNaWvJpf62bc2J8xvhO8u7REySdcY2do', sender='@namo:sibnsk.net', server_timestamp=1782996432895, decrypted=False, verified=False, sender_key=None, session_id=None, transaction_id=None, url='mxc://sibnsk.net/zwYAqRoIKqNCeEFCrcKCGscb', body='forest_sounds.flac')
-RoomMessageFile(source={'unsigned': {'membership': 'join'}, 'content': {'body': 'pgsocworkpay-flask_20260609_1704.7z', 'info': {'mimetype': 'application/x-7z-compressed', 'size': 2961234}, 'm.mentions': {}, 'msgtype': 'm.file', 'url': 'mxc://sibnsk.net/VonMMDvUDefrZMRHMftHipyh'}, 'origin_server_ts': 1782996768265, 'sender': '@namo:sibnsk.net', 'type': 'm.room.message', 'event_id': '$ibi4BGDj544zkTGhY0-q-GZb-JweFvOyRJXBUIVYsPw'}, event_id='$ibi4BGDj544zkTGhY0-q-GZb-JweFvOyRJXBUIVYsPw', sender='@namo:sibnsk.net', server_timestamp=1782996768265, decrypted=False, verified=False, sender_key=None, session_id=None, transaction_id=None, url='mxc://sibnsk.net/VonMMDvUDefrZMRHMftHipyh', body='pgsocworkpay-flask_20260609_1704.7z')
-RoomMessageAudio(source={'unsigned': {'membership': 'join'}, 'content': {'body': 'Voice message', 'info': {'duration': 6931, 'mimetype': 'audio/ogg', 'size': 12004}, 'm.mentions': {}, 'msgtype': 'm.audio', 'org.matrix.msc1767.audio': {'duration': 6931, 'waveform': [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0]}, 'org.matrix.msc1767.file': {'mimetype': 'audio/ogg', 'name': 'Voice message.ogg', 'size': 12004, 'url': 'mxc://sibnsk.net/IocSSaMvXDkCZOgQHqTJmvez'}, 'org.matrix.msc1767.text': 'Voice message', 'org.matrix.msc3245.voice': {}, 'url': 'mxc://sibnsk.net/IocSSaMvXDkCZOgQHqTJmvez'}, 'origin_server_ts': 1782996912468, 'sender': '@namo:sibnsk.net', 'type': 'm.room.message', 'event_id': '$KagU_DTSXfo5e642aFOkSALvHHBK5-9-bqbNtINnLbA'}, event_id='$KagU_DTSXfo5e642aFOkSALvHHBK5-9-bqbNtINnLbA', sender='@namo:sibnsk.net', server_timestamp=1782996912468, decrypted=False, verified=False, sender_key=None, session_id=None, transaction_id=None, url='mxc://sibnsk.net/IocSSaMvXDkCZOgQHqTJmvez', body='Voice message'
-async def on_message(room: nio.MatrixRoom, event):
-    # Игнорируем сообщения бота
-    if event.sender == self.client.user:
-        return
-
-"""
-
 
 
 class CStatistic(basis.CBasis):
@@ -112,9 +88,9 @@ class CStatistic(basis.CBasis):
 
             return ERROR_CODE
 
-
-    def add_user_stat(self, puser_id: int, proom_id: int, pstat: db.CStat) -> int:
-        """Добавляет новую запись статистики по человеку."""
+    """
+    def add_user_stat(self, proom_id: int, puser_id: int, pstat: db.CStat) -> int:
+        ""Добавляет новую запись статистики по человеку.""
 
         assert puser_id is not None, \
             "Assert: [statistic.add_user_stat] " \
@@ -130,10 +106,10 @@ class CStatistic(basis.CBasis):
         except SQLAlchemyError:
 
             return ERROR_CODE
-
+    """
 
     def add_user_to_base(self, puser_id: int, puser_name: str) -> int:
-        """Добавляет нового пользователя в БД и возвращает его ID."""
+        """"Добавляет нового пользователя в БД и возвращает его ID."""
 
         assert puser_id is not None, \
             "Assert: [statistic.add_user_to_base] " \
@@ -399,14 +375,14 @@ class CStatistic(basis.CBasis):
 
                         user_stat.files += 1
                     # *** Если информации о юзере нет в базе, добавляем, иначе апдейтим
-                    if user_stat is None:
+                    # if user_stat is None:
 
-                        self.add_user_stat(user_id, room_id, user_stat)
+                    #    self.add_user_stat(room_id, user_id, user_stat)
 
-                    else:
+                    # else:
 
-                        self.update_user_stat(user_id, room_id, user_stat)
-
+                    # self.update_user_stat(room_id, user_id, user_stat)
+                    self.database.commit_changes(user_stat)
                     result = True
         return result
 
@@ -465,9 +441,9 @@ class CStatistic(basis.CBasis):
                         answer = self.get_personal_information(proom_id, puser_name)
         return answer
 
-
-    def update_user_stat(self, puser_id: int, proom_id: int, pstatfields: dict):
-        """Изменяет запись статистики по человеку."""
+"""
+    def update_user_stat(self, proom_id: int, puser_id: int, pstat: db.CStat):
+        ""Изменяет запись статистики по человеку.""
 
         query = self.database.query_data(db.CStat)
         query = query.filter_by(fuserid=puser_id)
@@ -475,5 +451,5 @@ class CStatistic(basis.CBasis):
         stat: db.CStat = query.first()
         if stat:
 
-            stat.set_all_fields(pstatfields)
             self.database.commit_changes(stat)
+"""
